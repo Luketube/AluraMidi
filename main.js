b1e2b4cb55986eb74f3ff5032682767e94656d84
Lucas Farias <lucasfarias48@hotmail.com>
2022-02-23 10:40:45 -0300

    Adicionado funcionalidade com as teclas espaço e enter

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
 function ReproduzSom (idDoSom){
     document.querySelector(idDoSom).play();
 }
 
 for(i = 0; i < listaDeTeclas.length; i++){
-    const tecla = listaDeTeclas[contador]
+    
+    const tecla = listaDeTeclas[i]
     const instrumento = tecla.classList[1]
     const idAudio = `#som_${instrumento}`;
     
     tecla.onclick = function () {
         ReproduzSom(idAudio);
     }
-    contador = contador +1 ;
-}
 
+    tecla.onkeydown = function (evento){
+        if(evento.code === 'Space' || evento.code === 'Enter')
+        tecla.classList.add('ativa');
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
